6da5c60c44891d9cb4df480430a3ea11a13e259e
dmstjfgus <shfkddl934@naver.com>
2022-12-23 12:01:40 +0900

    ge products

--- model/product_upload.php
+++ model/product_upload.php
@@ -14,7 +14,7 @@ date_default_timezone_set("Asia/Seoul");
 $pro_name =  $_POST['product_name'];
 $pro_price =  $_POST['product_price'];
 $pro_desc =  nl2br($_POST['product_desc']); //nl2br : 엔터로 개행 시 br 태그 자동 입력
-$pro_desc =  addslashes($pro_desc); // 따옴표 중복 작성시 '/' 처리를 자동으로 실행
+// $pro_desc =  addslashes($pro_desc); // 따옴표 중복 작성시 '/' 처리를 자동으로 실행
 
 // 날짜 변수
 $pro_reg = date("Y-m-d H:i:s");
@@ -31,7 +31,7 @@ $allowed_exp = array("jpg", "jpeg", "png", "gif");
 $exp = array_pop(explode(".",$image_name)); // "."을 기준으로 뒷부분 문자열 반환
 
 // 파일 사이즈 제한 변수
-$limit = 50000;
+$limit = 5000000;
 
 // 확장자 필터링
 if(!in_array($exp, $allowed_exp)){// in_array : 두번째 파라미터 배열에 첫번째 파라미터 문자열이 있으면 true
@@ -40,17 +40,52 @@ if(!in_array($exp, $allowed_exp)){// in_array : 두번째 파라미터 배열에
 }
 
 // 파일 사이즈 제한 
-if($limit <= $image_size){
+if($limit < $image_size){
     echo json_encode(array("msg" => "사진 파일은 50MB를 넘을 수 없습니다."));
     exit();
 }
 
+$image_dir = $_SERVER['DOCUMENT_ROOT'].'/main_project/images/products/'; // 사진이 저장될 경로 저장
+// echo json_encode(array("msg" => $image_dir));
+
 // 이미지 업로드 : * 파일 업로드 시 파일 자체는 지정한 디렉토리 안으로 가고, DBDPSMS ㅊ파일이름 문자열이 지정된다.
 if($image_name && !$image_error){
-
+    // move_uploaded_file(파일 임시 이름, 이동할위치)
+    $upload_file = $image_dir.$image_name;
+    if(!move_uploaded_file($image_tmp_name, $upload_file)){
+        // echo json_encode(array("msg" => "사진이 업로드 되지 않았습니다"));
+        exit();
+    }
 } else {
     $image_name  = "";
 }
+
+// 접속 정보 로드
+include_once $_SERVER['DOCUMENT_ROOT'].'/main_backend/connect/dbconn.php';
+
+ // sql 명령어 입력
+ $sql = "INSERT INTO spl_products (pro_name, pro_pri, pro_desc, pro_img,  pro_reg) VALUES (?,?,?,?,?)";
+
+ // // stmt init 참조 :https://www.w3schools.com/php/func_mysqli_stmt_init.asp
+ $stmt = $conn->stmt_init();
+
+ // echo json_encode(array("name" => var_dump($stmt)));
+
+ if(!$stmt->prepare($sql)){
+   http_response_code(400);
+   echo json_encode(array("msg" => "Database insert fail"));
+ }
+
+ $stmt -> bind_param("sssss", $pro_name, $pro_price, $pro_desc, $image_name, $pro_reg);
+ $stmt -> execute();
+
+ if($stmt->affected_rows > 0){
+   http_response_code(200);
+   echo json_encode(array("msg" => "입력이 완료되었습니다."));
+ }else{
+   http_response_code(400);
+   echo json_encode(array("msg" => "입력에 실패했습니다."));
+ }
  
 //  echo json_encode(array("name" => $pro_name, "price" => $pro_price, "desc" => $pro_desc, "reg" => $pro_reg, "img_name" => $image_name, "img_tmp_name" => $image_tmp_name, "img_type" => $image_type, "img_size" => $image_size, "img_error" => $image_error, "exp" => $exp));
  
